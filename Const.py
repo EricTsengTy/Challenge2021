ab5aaf2f85e2b59e859d0a098896d76492608265
brian <brianlee0415@gmail.com>
2021-06-28 20:46:04 +0800

    4 player

--- Const.py
+++ Const.py
@@ -3,12 +3,12 @@ import pygame as pg
 # model
 FPS = 60 # frame per second
 GAME_LENGTH = 30 * FPS
-PLAYER_INIT_POSITION = [pg.Vector2(200, 400), pg.Vector2(600, 400)]
-PLAYER_NUMBER=2
+PLAYER_INIT_POSITION = [pg.Vector2(200, 400), pg.Vector2(300, 400), pg.Vector2(400, 400), pg.Vector2(500, 400)]
+PLAYER_NUMBER = 4
 PLAYER_RADIUS = 40
 PLAYER_SPEED = 100
 PLAYER_JUMP_SPEED = 100
-PLAYER_GRAVITY = 100
+PLAYER_GRAVITY = 150
 
 DIRECTION_TO_VEC2 = {
     'up': pg.Vector2(0, -1),
@@ -31,7 +31,7 @@ WINDOW_CAPTION = 'Challenge 2020 Homework'
 WINDOW_SIZE = (800, 800)
 ARENA_SIZE = (800, 800)
 BACKGROUND_COLOR = pg.Color('black')
-PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta')]
+PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta'), pg.Color('cyan'), pg.Color('white')]
 
 
 # controller
