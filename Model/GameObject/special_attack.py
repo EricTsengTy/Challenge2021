b3cadf771f229fff220e2f2e7e960dc220471030
brian <brianlee0415@gmail.com>
2021-07-04 11:38:39 +0800

    change where to store attacks

--- Model/GameObject/special_attack.py
+++ Model/GameObject/special_attack.py
@@ -3,7 +3,39 @@ from pygame import transform
 import Const
 from pygame.math import Vector2
 from Model.GameObject.basic_game_object import Basic_Game_Object
-from Model.GameObject.basic_attack_object import *
+
+class Basic_Attack_Object(Basic_Game_Object):
+    def __init__(self, model, attacker_id, position, direction, damage, name):
+        super().__init__(model, position.x, position.y, 1, 1)
+        self.name = name
+        self.attacker_id = attacker_id
+        self.speed = direction.normalize() * Const.ARROW_SPEED
+        self.damage = damage
+        
+    def tick(self):
+        self.basic_tick()
+        for player in self.model.players:
+            if self.attacker_id != player.player_id\
+                and player.rect.collidepoint(self.center.x, self.center.y):
+                player.be_special_attacked(self)
+                self.kill()
+
+class Arrow(Basic_Attack_Object):
+    def __init__(self, model, attacker_id, position, direction, damage):
+        super().__init__(model, attacker_id, position, direction, damage, 'Arrow')
+
+class Bug(Basic_Attack_Object):
+    def __init__(self, model, attacker_id, position, direction, damage):
+        super().__init__(model, attacker_id, position, direction, damage, 'Bug')
+        self.obey_gravity = True
+        self.gravity = Const.BUG_GRAVITY
+
+class Coffee(Basic_Attack_Object):
+    def __init__(self, model, attacker_id, position, direction, damage):
+        super().__init__(model, attacker_id, position, direction, damage, 'Coffee')
+        self.obey_gravity = True
+        self.gravity = Const.COFFEE_GRAVITY
+
 class Dos(Basic_Game_Object):
     def __init__(self, model, attacker, target):
         super().__init__(model, attacker.center.x, attacker.center.y, 1, 1)
@@ -23,7 +55,6 @@ class Dos(Basic_Game_Object):
         self.timer -= 1
         if self.rounds<=0:
             self.kill()
-
 
 class Ddos(Basic_Game_Object):
     def __init__(self, model, attacker, target):
@@ -47,7 +78,7 @@ class Ddos(Basic_Game_Object):
         self.timer -= 1
         if self.rounds<=0:
             self.kill()
-            
+
 class Throw_Bug(Basic_Game_Object):
     def __init__(self, model, attacker, target):
         super().__init__(model, attacker.center.x, attacker.center.y, 1, 1)
