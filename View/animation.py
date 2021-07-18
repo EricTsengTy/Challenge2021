839146ebbf95666abd656c1de127975faf270709
Li Andrew <andruw0205@gmail.com>
2021-07-18 19:15:49 +0800

    change copy to reference and some others improvement

--- View/animation.py
+++ View/animation.py
@@ -1,13 +1,44 @@
 import pygame as pg
 import numpy as np
+from View.utils import scale_surface, load_image, resize_surface
 import Const
 import random
 
 class Animation_base():
-    def __init__(self, delay_of_frames):
+    def __init__(self, delay_of_frames, **pos):
         self._timer = 0
         self.expired = False
+
+class Animation_raster():
+    frames = tuple()
+
+    @classmethod
+    def init_convert(cls):
+        cls.frames = tuple( _frame.convert_alpha() for _frame in cls.frames)
+
+    def __init__(self, delay_of_frames, expire_time, **pos):
+        self._timer = 0
         self.delay_of_frames = delay_of_frames
+        self.frame_index_to_draw = 0
+        self.expire_time = expire_time
+        self.expired = False
+    
+    def update(self):
+        self._timer += 1
+
+        if self._timer == self.expire_time:
+            self.expire = True
+        elif self._timer % self.delay_of_frames == 0:
+            self.frame_index_to_draw = (self.frame_index_to_draw + 1) % len(self.frames)
+
+    def draw(self, screen, update=True):
+        screen.blit(
+            self.frames[self.frame_index_to_draw],
+            self.frames[self.frame_index_to_draw].get_rect(**self.pos),
+        )
+        
+        if update: self.update()
+
 
 class Animation_hello_world(Animation_base):
     
@@ -56,68 +87,62 @@ class Animation_hello_world(Animation_base):
     '''
 
 class Greeting_from_prog(Animation_base):
-    
     def __init__(self,delay_of_frames):
         super().__init__(delay_of_frames)
         self.expired_time = 80
-        font = pg.font.SysFont(None, 200)
-        self.text_surface = font.render('Hello World!', True, (0, 0, 0))
+        head_font = pg.font.SysFont(None, 200)
+        self.text_surface = head_font.render('Hello World!', True, (0, 0, 0))
         self.text_surface = self.text_surface.convert_alpha()
 
     def rolling(self,source_arr):
         return np.roll(source_arr,self._timer*-10,axis=1)
 
-    def RGB_shift(self,source_arr):
-        R = source_arr[:,:,0]
-        G = source_arr[:,:,1]
-        B = source_arr[:,:,2]
-        result_arr = np.zeros((*self.canvas_size,3))
+    def RGB_shift(self,pixel_arr):
+        R = pixel_arr[:,:,0]
+        G = pixel_arr[:,:,1]
+        B = pixel_arr[:,:,2]
         
         a,b,c = random.randint(-13,13),random.randint(-13,13),random.randint(-13,13)
-        if a > 0   : result_arr[a:,:,0] = R[:-a,:]
-        elif a < 0 : result_arr[:a,:,0] = R[-a:,:]
-        if b > 0   : result_arr[b:,:,1] = G[:-b,:]
-        elif b < 0 : result_arr[:b,:,1] = G[-b:,:]
-        if c > 0   : result_arr[c:,:,2] = B[:-c,:]
-        elif c < 0 : result_arr[:c,:,2] = B[-c:,:]
-        
-        return result_arr
+        if a > 0   : pixel_arr[a:,:,0] = R[:-a,:]
+        elif a < 0 : pixel_arr[:a,:,0] = R[-a:,:]
+        if b > 0   : pixel_arr[b:,:,1] = G[:-b,:]
+        elif b < 0 : pixel_arr[:b,:,1] = G[-b:,:]
+        if c > 0   : pixel_arr[c:,:,2] = B[:-c,:]
+        elif c < 0 : pixel_arr[:c,:,2] = B[-c:,:]
     
-    def shifting(self,source_arr):
-        result_arr = np.zeros((*self.canvas_size,3))
-        result_arr[:,:,:] = source_arr[:,:,:]
-        for _ in range(1):
-            stripe_width = 20
-            stripe_pos = 2
-            stripe_amount = 5
-            result_arr[stripe_amount:,stripe_pos:stripe_pos+stripe_width] = source_arr[:-stripe_amount,stripe_pos:stripe_pos+stripe_width]
+    def shifting(self,pixel_arr):
+        for _ in range(5):
+            stripe_width = random.randint(20,30)
+            stripe_pos = random.randint(0,self.canvas_size[1]-1)
+            stripe_amount = random.randint(5,30)
+            pixel_arr[stripe_amount:,stripe_pos:stripe_pos+stripe_width] = pixel_arr[:-stripe_amount,stripe_pos:stripe_pos+stripe_width]
         
-        for _ in range(0):
+        for _ in range(10):
             stripe_width = random.randint(0,10)
             stripe_pos = random.randint(0,self.canvas_size[1]-1)
             stripe_amount = random.randint(5,30)
-            result_arr[stripe_amount:,stripe_pos:stripe_pos+stripe_width] = source_arr[:-stripe_amount,stripe_pos:stripe_pos+stripe_width]
-        return result_arr
-    
+            pixel_arr[stripe_amount:,stripe_pos:stripe_pos+stripe_width] = pixel_arr[:-stripe_amount,stripe_pos:stripe_pos+stripe_width]
+
+    def scrollY(self, screen, amount):
+        width, height = screen.get_size()
+        copySurf = screen.convert()
+        screen.blit(copySurf, (0, amount))
+        if amount < 0:
+            screen.blit(copySurf, (0, height + amount), (0, 0, width, -amount))
+        else:
+            screen.blit(copySurf, (0, 0), (0, height-amount, width, amount))
+
     def update(self):
         self._timer += 1
-
         if self._timer == self.expired_time:
             self.expired = True
-
-    def Hello_world(self,screen):
-        HW_pos = (100,250)
-        screen.blit(self.text_surface, HW_pos)
 
     def draw(self, screen, update):
         if update: self.update()
         self.canvas_size = screen.get_size()
+        self.scrollY(screen,(self._timer*-10)) # default to scroll -10 pixel
+        screen.blit(self.text_surface, self.text_surface.get_rect( center =  screen.get_rect().center ))
 
-        source_arr = pg.surfarray.array3d(screen)
-        source_arr = self.rolling(source_arr)
-        
-        source_arr = self.RGB_shift(source_arr)
-        source_arr = self.shifting(source_arr)
-        result = pg.surfarray.make_surface(source_arr)
-        screen.blit(result,(0,0))
-        self.Hello_world(screen)
+        source_arr = pg.surfarray.pixels3d(screen)
+        self.RGB_shift(source_arr)
+        self.shifting(source_arr)
