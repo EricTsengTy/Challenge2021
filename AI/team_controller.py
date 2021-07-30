8b1a5f93ca83a5b0b3db50a36f1a9e26ef5db144
b09902056 <b09902056@csie.ntu.edu.tw>
2021-07-30 10:58:55 +0800

    dicide

--- AI/team_controller.py
+++ AI/team_controller.py
@@ -3,4 +3,21 @@ class TeamAI ():
         self.helper = helper
 
     def decide (self):
+        decision = None
+        if decision is None:
+            decision = self.special_attack()
+        if decision is None:
+            decision = self.walk_to_EXE()    
+        if decision is None:
+            decision = self.attack()
+        if decision is None:
+            decision = self.walk_to_nearest_player()
+    
+    def special_attack(self):
+        pass
+    def walk_to_EXE(self):
+        pass
+    def attack(self):
+        pass
+    def walk_to_nearest_player(self):
         pass
