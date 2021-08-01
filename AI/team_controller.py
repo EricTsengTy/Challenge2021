63878bfc17999f431784cbcab9373d6b89dcd8dd
b09902056 <b09902056@csie.ntu.edu.tw>
2021-08-01 22:33:58 +0800

    double_jump

--- AI/team_controller.py
+++ AI/team_controller.py
@@ -30,7 +30,8 @@ class TeamAI ():
     def decide (self):
         decision = None
         if decision is None:
-            decision = self.helper.double_jump()     
+            decision = self.helper.double_jump()       
+        '''
         if decision is None:
             decision = self.special_attack()
         if decision is None:
@@ -39,6 +40,7 @@ class TeamAI ():
             decision = self.attack()
         if decision is None:
             decision = self.walk_to_nearest_player()
+        '''
         return decision
 
     def special_attack(self):
