ec13fdb54acad939e9e6a9e901f1f12e2df3ca1d
b09902056 <b09902056@csie.ntu.edu.tw>
2021-08-02 13:23:02 +0800

    dict

--- AI/team_controller.py
+++ AI/team_controller.py
@@ -7,6 +7,8 @@ AI_DIR_DOUBLE_JUMP       = 5
 AI_DIR_ATTACK            = 6
 AI_DIR_SPECIAL_ATTACK    = 7
 
+
+
 '''
 FAN,
 LIGHTNING,
@@ -23,15 +25,21 @@ FOLDER_UNUSED,
 CHARGE 
 '''
 
+AI_DIR = {'left': False, 'right': False, 'jump': False, 'attack': False, 'special_attack': False}   
 class TeamAI ():
     def __init__ (self, helper):
         self.helper = helper
 
     def decide (self):
-        decision = None
-        if decision is None:
-            decision = self.helper.double_jump()       
+        for dir in AI_DIR:
+            AI_DIR[dir] = False   
+        self.special_attack()
+        self.attack()
+        #self.walk_to_EXE()
+        #self.walk_to_nearest_player() # if no walk_to_EXE
+        return AI_DIR
         '''
+        decision = None
         if decision is None:
             decision = self.special_attack()
         if decision is None:
@@ -40,14 +48,17 @@ class TeamAI ():
             decision = self.attack()
         if decision is None:
             decision = self.walk_to_nearest_player()
-        '''
         return decision
+        '''
 
     def special_attack(self):
         if self.helper.get_can_use_special_attack() :
-            return AI_DIR_SPECIAL_ATTACK
-        return None
-        
+            AI_DIR['special_attack'] = True
+
+    def attack(self):
+        if self.helper.get_can_common_attack() and self.helper.get_if_player_in_attack_range():
+            AI_DIR['attack'] = True
+
     def walk_to_EXE(self):
     
         if self.helper.get_nearest_specific_item_position("EXE") == None:
@@ -70,10 +81,7 @@ class TeamAI ():
         return self.helper.walk_to_specific_position(nearest_EXE)
         '''
 
-    def attack(self):
-        if self.helper.get_can_common_attack() and self.helper.get_if_player_in_attack_range():
-            return AI_DIR_ATTACK
-        return None
+
 
     def walk_to_nearest_player(self):
         pos_y = self.helper.get_self_position()[1]
