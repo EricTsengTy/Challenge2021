fd3227c41ff59660130a1b70b84c328d2d5fcb1b
vppyw <vincent01pao@gmail.com>
2022-01-19 18:15:51 +0800

    Modify some variables to Const

--- Model/GameObject/player.py
+++ Model/GameObject/player.py
@@ -127,7 +127,7 @@ class Player(Basic_Game_Object):
             self.keep_item_type = item_type
             self.special_attack_timer = 0
         elif item_type == 'EXE':
-            self.score += 300
+            self.score += Const.SCORE_HELLO_WORLD
             self.model.ev_manager.post(EventHelloWorld())
         elif item_type == 'USB':
             State.infect(self.state)
